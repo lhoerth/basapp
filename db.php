6de3c91d2e2b4ae6478cb26d110085aa88dbe331
Casey Morris <casemorris@hotmail.com>
2015-02-26 21:39:57 -0800

    This is where your db credentials go. Don't normally store it on github.

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+    $username = "caseym_monkey"; //Use your database username
+    $password = "Midget11";  //Use your database password
+    $hostname = "localhost"; //keep the same
+    $dbname="caseym_BAS"; //Change to your own database name
+    
+    try {
+      //database object
+      $dbh = new PDO ("mysql:host=$hostname;
+                      dbname=$dbname", $username, $password);
+      echo 'connected to database';
+    }
+    catch(PDOException $e) {
+      echo $e->getMessage();
+    }
+?>
