5104027ad23e538f175214cdfc28d82ed1a23ed4
shristhy <shristhynaidu@Shristhys-MacBook-Pro.local>
2015-02-26 20:30:06 -0800

    summary

--- form5.php
+++ form5.php
@@ -3,22 +3,90 @@
 session_start();
 //*** Start the buffer
 ob_start();
-
-/* // DEBUGGING - OUTPUT SESSION 
-echo "<pre>";
-print_r($_POST);
-print_r($_SESSION);
-echo "</pre>";
-*/
-
-//Goes back to previous page
-	if(!isset($_SESSION['Submit4'])){
-		 header("Location: index.php?page=form1");
-		 exit;
-	}
-
+echo"<prev>";
+var_dump($_SESSION);
+echo"</prev>";
 ?>
 <div class="container">
+    <h3>Summary</h3>
+    <p>Please verify your information and submit</p>
+    <?php
+    echo "<b>First: </b>". $_SESSION['first'];
+    echo "<br><b>Last: </b>". $_SESSION['last'];
+     echo "<br><b>Student ID: </b>". $_SESSION['sid'];
+    echo "<br><b>Email Address: </b>". $_SESSION['email'];
+    echo "<br><b>Phone Number: </b>". $_SESSION['phone'];
+    $status = " <br><b>Student status:</b>";
+ 
+   if ($_SESSION['is'] == 'international') {
+    $status .= "International Student," ;
+   }
+   
+     if ($_SESSION['veteran'] == 'veteran') {
+    $status .=" Veteran," ;
+   }
+
+   if ($_SESSION['rs'] == 'running-start') {
+    $status .=" Running Start Student" ;
+   }
+   echo($status);
+   
+   if($_SESSION['degree']=='network'){
+    echo "<br><b>Program interested:</b> Network & Security";
+   }
+   
+    if($_SESSION['degree']=='software'){
+    echo "<br><b>Program interested:</b> Software Development";
+   }
+   
+   if($_SESSION['degree']=='ud'){
+    echo "<br><b>Program interested: </b> Undecided";
+   }
+   
+   
+   
+   if(isset($_SESSION['netReq'])){
+    $require = "<br><b>Prequisites Completed:</b>";
+    foreach($_SESSION['netReq'] as $info){
+        $require .= $info;
+    }
+    echo $require;
+   }
+   
+   
+         if(isset($_SESSION['softReq'])){
+    $require = "<br><b>Prequisites Completed:</b>";
+    foreach($_SESSION['softReq'] as $info){
+        $require .= $info;
+    }
+    echo $require;
+         }
+    
+   
+   if(isset($_SESSION['comment'])){
+    echo "<br><b>IT class taken: </b>" . $_SESSION['comment'];
+   }
+ 
+echo "<br><b>Education: </b>" . $_SESSION['lvlEducation'];
+echo "<br><b>Credits: </b>" . $_SESSION['collegeCredits'];
+
+
+   
+   
+ 
+   
+   
+   
+    
+    
+    
+    
+    ?>
+    
+    
+    
+    
+    
 
 </div>
 
